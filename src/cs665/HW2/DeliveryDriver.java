b52ac2dc523a7d58569aab541f8e6a7390f2000c
Jingfan-Hu <113561009+Jingfan-Hu@users.noreply.github.com>
2023-12-05 14:49:44 -0500

    Delete src/cs665/HW2 directory

--- src/cs665/HW2/DeliveryDriver.java
+++
@@ -1,5 +0,0 @@
-package cs665.HW2;
-
-public interface DeliveryDriver {
-    void update(DeliveryRequest deliveryRequest);
-}
--- src/cs665/HW2/Shop.java
+++
@@ -1,7 +0,0 @@
-package cs665.HW2;
-
-public interface Shop {
-    void addObserver(DeliveryDriver observer);
-    void removeObserver(DeliveryDriver observer);
-    void notifyObservers(DeliveryRequest deliveryRequest);
-}
